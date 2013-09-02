1ff08d42f1e4a5e026a6086fe384664bc0114ece
taniya <taniya@bbt757.com>
2013-09-02 17:49:50 +0900

    getMorikoroneの雛形作成

---
+++ src/kenta/Kenta.java
@@ -0,0 +1,18 @@
+package kenta;
+
+public class Kenta {
+	
+	public void getMorikorone(boolean flg) {
+		//TODO 後でやる
+		say("金曜日にビール飲みにいきませんか？");
+		return;
+	}
+	public void goGokon() {
+		say("いついくの？");
+		return;
+	}
+	private void say(String message) {
+		System.out.println(message);
+	}
+	
+}
---
+++ src/kenta/Main.java
@@ -0,0 +1,17 @@
+package kenta;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO kenta method stub
+		Kenta kenta = new Kenta();
+		kenta.getMorikorone(true);
+		
+		// TODO 合コン
+		kenta.goGokon();
+	}
+
+}
